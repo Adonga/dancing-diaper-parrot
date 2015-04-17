107ff721ecf5516d6af2e7f62d8ca5850d9afa64
PStang <paul@stacom.de>
2015-04-17 20:37:37 +0200

    added GameState and added some variables to GameSingelton

--- Assets/Script/CGameSingelton.cs
+++ Assets/Script/CGameSingelton.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class CGameSingelton : Singelton<CGameSingelton>
 {
-
+    public bool alive;
+    public CGameState currentGameState;
+    public CGameState previousGameState;
 	// Use this for initialization
 	void Start () {
-	
+        GameObject.DontDestroyOnLoad(this.gameObject);
 	}
 	
 	// Update is called once per frame
--- Assets/Script/CPlayer.cs
+++ Assets/Script/CPlayer.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class CPlayer : MonoBehaviour {
 
 	// Use this for initialization
+    Vector3 position = new Vector3(0,0,0);
 	void Start () {
-	
+        position = this.transform.localPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKey(KeyCode.D)) {
+            position.x += 0.2f;
+            this.transform.localPosition = position;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            position.x -= 0.2f;
+            this.transform.localPosition = position;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.y += 0.2f;
+            this.transform.localPosition = position;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            position.y -= 0.2f;
+            this.transform.localPosition = position;
+        }
 	}
 }
