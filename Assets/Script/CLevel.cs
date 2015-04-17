1136be617f774fabe8a65cdc3b695e9cb0f69dde
PStang <paul@stacom.de>
2015-04-17 20:43:37 +0200

    sth. so we can hardcode a level without making a new scene

---
+++ Assets/Script/CLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CLevel : MonoBehaviour {
+
+	// Use this for initialization
+    void makeSth() { 
+//          GameObject prefab = GameSingelton.instance.sth;
+//          GameObject clone = (GameObject)Instantiate(prefab, new Vector3(Random.Range(0, 10) * 2f, -4 + Random.Range(0, 2) * 0.5f, 0), transform.rotation);
+//          clone.transform.localScale = new Vector3(0,0,0);
+    }
+    void Start () {
+        makeSth();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
