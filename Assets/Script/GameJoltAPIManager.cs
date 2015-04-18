a3c0c74620c87953fbb2b898e4c463138237479d
PStang <paul@stacom.de>
2015-04-18 18:49:27 +0200

    starting working on mainmenu

---
+++ Assets/Script/GameJoltAPIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameJoltAPIManager : MonoBehaviour
+{
+    public int gameID;
+    public string privateKey;
+    public string userName;
+    public string userToken;
+    GJAPI GameJoltAPI   ;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        gameID = 60912;
+        userName = "Adonga";
+        userToken = "4a6f2d";
+        privateKey = "2465d53c5d34f2505eb38a16e26ad03e";
+        GJAPI.Init(gameID, privateKey);
+        GJAPIHelper.Users.ShowLogin();
+    }
+    
+    public void ShowTrophie() 
+    {
+        GJAPIHelper.Trophies.ShowTrophies();
+    }
+    public void showScore() 
+    {
+        GJAPIHelper.Scores.ShowLeaderboards();
+    }
+}
